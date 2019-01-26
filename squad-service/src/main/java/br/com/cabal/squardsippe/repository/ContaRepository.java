3622af5330323b34e065d522fb9521277222c240
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2019-01-26 15:44:03 -0200

    criado DTOS api

--- squad-service/src/main/java/br/com/cabal/squardsippe/repository/ContaRepository.java
+++ squad-service/src/main/java/br/com/cabal/squardsippe/repository/ContaRepository.java
@@ -3,7 +3,10 @@ package br.com.cabal.squardsippe.repository;
 import br.com.cabal.squardsippe.model.Conta;
 import br.com.cabal.squardsippe.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
