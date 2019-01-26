8064de61d0df65cd5ab9fc3a32edbdeb79d5b12e
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2019-01-26 11:28:56 -0200

    Criando o modulo service

---
+++ squad-service/src/main/java/br/com/cabal/squardsippe/SquardSippeApplication.java
@@ -0,0 +1,14 @@
+package br.com.cabal.squardsippe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquardSippeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquardSippeApplication.class, args);
+    }
+
+}
+
